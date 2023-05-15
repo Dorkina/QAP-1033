d5c17e0c6bf01c2a169b879c21e4c2423cf817a7
Dorkina <ekaterina.zharkova.91@gmail.com>
2023-05-15 23:43:26 +0300

    Итоговое задание 18 модуль

---
+++ bot/config.py
@@ -0,0 +1,7 @@
+TOKEN = "6206420737:AAHoDghQeaLkQoAD-QJ_Z_JxB1cei44UBuM"
+
+KEYS = {'биткоин': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        'евро': 'EUR',
+        'рубль': 'RUB'}
